1d29c1d5069f5671891a35d1fb7741f7d77713a1
Skillfulspade <matthew92902@gmail.com>
2026-05-20 20:47:12 -0400

    Creating systems

---
+++ Assets/_Scripts/Developed/Core/Units/UnitGravity.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+/* ScriptName:      UnitGravity.cs
+ * Author(s):       Matthew Perry
+ * Last Updated:    5-20-2026
+ * Description:     This is a script that adds Gravity to a Unit.
+ */
+
+namespace OneThread.Core.Units
+{
+    public class UnitGravity : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/_Scripts/Developed/ScriptTemplate.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+/* ScriptName:      Template.cs
+ * Author(s):       Matthew Perry
+ * Last Updated:    X-XX-20XX
+ * Description:     This is a script that
+ * is meant to be the template that lays
+ * out the way all my scripts look like!
+ */
+
+namespace NameSpaceName.SubNameSpaceName
+{
+    public class ScriptTemplate
+    {
+        #region Events:
+        #endregion Events
+
+        #region Public Variables:
+        #endregion Public Variables
+
+        #region Private Variables:
+        #endregion Private Variables
+
+        #region Unity Behaviors:
+        #endregion Unity Behaviors
+
+        #region Private Behaviors:
+        #endregion Private Behaviors
+
+        #region Public Behaviors:
+
+        #region Setters:
+        #endregion Setters
+
+        #region Getters:
+        #endregion Getters
+
+        #endregion Public Behaviors
+    }
+}
